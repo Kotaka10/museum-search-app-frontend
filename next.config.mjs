95290bb9a97de7c4d33161783d04a815cc556c10
古宮隆宏 <tennis10sao03@outlook.jp>
2025-09-16 22:41:15 +0900

    fix: next.config.mus

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
-/** @type {import('next').NextConfig} */
-export const images = {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "museum-search-app-theta.vercel.app",
-      pathname: "/images/**",
-    },
-    {
-      protocol: "https",
-      hostname: "another.com",
-    },
-  ],
-};
+import { defineConfig } from 'next';
+
+export default defineConfig({
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "museum-search-app-theta.vercel.app",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "another.com",
+      },
+    ],
+  },
+});
